1de00629b7cf98895e8f28929c8c8db9d6d4119a
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-01-27 12:29:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,15 @@
-{
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "Chris' Pancakes and Dining",
-        "Cuisine Type": "Best Breakfast",
-        "Address": "5980 Southwest Ave",
-        "City": "St. Louis, MO 63139"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.28939962387085,
-          38.60944288867626
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "Thai Table",
-        "Cuisine Type": "Best Thai",
-        "Address": "7403 Manchester Rd",
-        "City": "Maplewood, MO 63143"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.32014846801756,
-          38.61259194753989
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "Mai Lee",
-        "Cuisine Type": "Best Vietnamese",
-        "Address": "8396 Musick Memorial Dr",
-        "City": "Brentwood, MO 63144"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.33746480941772,
-          38.62757454386942
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "Bogart's Smokehouse",
-        "Cuisine Type": "Best Barbeque",
-        "Address": "1627 S 9th St",
-        "City": "St. Louis, MO 63104"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.20273208618164,
-          38.61089115853795
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "La Catrina",
-        "Cuisine Type": "Best Mexican",
-        "Address": "5220 Hampton Ave",
-        "City": "St. Louis, MO 63109"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.29265582561493,
-          38.58471928609339
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "Balkan Treat Box",
-        "Cuisine Type": "Best Ethnic/European",
-        "Address": "8103 Big Bend Blvd",
-        "City": "Webster Groves, MO 63119"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.3378564119339,
-          38.59212426312337
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Restaurants": "Tree House",
-        "Cuisine Type": "Best Vegan/Vegetarian",
-        "Address": "3177 S Grand Blvd",
-        "City": "St. Louis, MO 63118"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.24294376373291,
-          38.60017830017862
-        ]
-      }
-    }
-  ]
-}
+var map = L.map('mapid').setView([38.6,-90.2],11);
+
+//load tile layer
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
+
+// load GeoJSON from an external file
+$.getJSON("https://raw.githubusercontent.com/adam-camp/stl_food_from_geojson/main/places.geojson",function(data){
+L.geoJson(data).addTo(map);
+});
